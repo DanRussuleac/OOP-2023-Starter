725fe4bc816a1dd4bfd7f057b54329a603608430
DanRussuleac <drussuleac@gmail.com>
2023-04-26 16:58:26 +0100

    comments for word class

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -3,30 +3,37 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 public class Word {
+    // Instance variables for the word and its follows ArrayList
     private String word;
     private ArrayList<Follow> follows;
 
+    // Constructor to initialize the word and create an empty follows ArrayList
     public Word(String word) {
         this.word = word;
         follows = new ArrayList<>();
     }
 
+    // Getter for the word
     public String getWord() {
         return word;
     }
 
+    // Getter for the follows ArrayList
     public ArrayList<Follow> getFollows() {
         return follows;
     }
 
+    // Setter for the word
     public void setWord(String word) {
         this.word = word;
     }
 
+    // Method to add a follow object to the follows ArrayList
     public void addFollow(Follow follow) {
         follows.add(follow);
     }
 
+    // Method to find a follow object in the follows ArrayList by its word
     public Follow findFollow(String str) {
         for (Follow follow : follows) {
             if (follow.getWord().equals(str)) {
@@ -36,6 +43,7 @@ public class Word {
         return null;
     }
 
+    // Overriding the toString method for custom string representation
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
